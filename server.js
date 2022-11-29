56d4c5c9f18fa81aab7293eaccde55f76ebd0321
noebrown <noejbrown@gmail.com>
2022-11-28 20:04:32 -0500

    fixed server.js issues

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { roll } from './lib/roll.js';
 import minimist from 'minimist';
 import express from 'express';
 
-//define const(s)
+// define const(s)
 const app = express()
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000
@@ -12,7 +12,7 @@ const port = args.port || 5000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// parsing
 app.get('/app/', function (req, res) {
     res.send('200 OK');
 })
@@ -23,20 +23,20 @@ app.get('/app/roll/', function (req, res) {
 
 app.get('/app/roll/*/', function (req, res) {
     let url = req.url;
-    let myArray = url.split("/");
-    res.send(roll(myArray[3], 2, 1))
+    let array = url.split("/");
+    res.send(roll(array[3], 2, 1))
 })
 
 app.get('/app/roll/*/*/', function (req, res) {
     let url = req.url;
-    let myArray = url.split("/");
-    res.send(roll(myArray[3], myArray[4], 1))
+    let array = url.split("/");
+    res.send(roll(array[3], array[4], 1))
 })
 
 app.get('/app/roll/*/*/*/', function (req, res) {
     let url = req.url;
-    let myArray = url.split("/");
-    res.send(roll(myArray[3], myArray[4], myArray[5]))
+    let array = url.split("/");
+    res.send(roll(array[3], array[4], array[5]))
 })
 
 
@@ -48,9 +48,7 @@ app.post('/app/roll/', function(req, res) {
     res.send(roll(req.body.sides, req.body.dice, req.body.rolls));
 });
 
-app.listen(port, () => {
-    // console.log("Server listening on port " + port + ".")
-})
+app.listen(port)
 
 /*
  testers
